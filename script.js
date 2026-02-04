e78937cbd269b4fa603bbffaa44b0ae884625fc4
andresspinedap-ui <lokolink449@gmail.com>
2026-02-04 15:36:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function huir() {
+  document.getElementById("resultado").style.display = "block";
+}
+
+function aceptar() {
+  document.getElementById("escena-1").classList.remove("activa");
+  document.getElementById("escena-2").classList.add("activa");
+}
