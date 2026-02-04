a24ab53d91106b0e72495fbbe4ffea1668df3086
andresspinedap-ui <lokolink449@gmail.com>
2026-02-04 11:57:32 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function huir() {
-  const resultado = document.getElementById("resultado");
-  resultado.style.display = "block";
-}
-
-function aceptar() {
-  document.getElementById("escena-1").classList.remove("activa");
-  document.getElementById("escena-2").classList.add("activa");
-}
